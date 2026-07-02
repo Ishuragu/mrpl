85a379e8970381c33c0f5c9cb4cca589aff060df
Iswarya <ishuragu@gmail.com>
2026-07-02 18:54:13 +0100

    updated transported code in email.js

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -20,16 +20,18 @@ exports.handler = async (event) => {
     }
 
     const transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: Number(process.env.SMTP_PORT),
-      secure: process.env.SMTP_PORT == "465",
+      host: "smtp.titan.email",
+      port: 587,
+      secure: false,
+      requireTLS: true,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
       },
+      tls: {
+        rejectUnauthorized: false,
+      },
     });
-    await transporter.verify();
-    console.log("SMTP connection verified");
 
     const mailOptions = {
       from: `"Mettalic Group Website" <${process.env.SMTP_USER}>`,
