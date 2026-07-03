a81f2f6b8c7f6a64e0357c0a0822d4bb567025fe
Iswarya <ishuragu@gmail.com>
2026-07-03 18:28:04 +0100

    send email js update

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -20,9 +20,10 @@ exports.handler = async (event) => {
     }
 
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
+      host: process.env.SMTP_HOST,
+      port: Number(process.env.SMTP_PORT),
       secure: false,
+      requireTLS: true,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
